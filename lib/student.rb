f233248ea8660e6c0f8f3eb85a63b68b53c2be68
Michael Boroski <mjboroski@gmail.com>
2018-05-21 18:50:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :id, :name, :grade
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(id=nil, name, grade)
     @id=id
@@ -34,6 +35,31 @@ class Student
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
     @id=DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+
+  def create(name:, grade:)
+    student=Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    new_student=self.new
+    new_student.id=row[0]
+    new_student.name=row[1]
+    new_student.grade=row[2]
+    
+  end
+
+  def self.find_by_name(name)
+    sql= "SELECT * FROM students WHERE name=?"
+    result=DB[:conn].execute(sql, name)[0]
+    Student.new(result[0],result[1],result[2])
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
 end
